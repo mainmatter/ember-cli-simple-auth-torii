7ecd69952cf9f4b7ee4c36e72576133d6cef30a5
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-09-08 15:47:35 +0200

    cleanup

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 module.exports = {
   name: 'Ember CLI Simple Auth torii',
 
-  blueprintsPath: function() {
-    return path.join(__dirname, 'blueprints');
-  },
-
   included: function(app) {
     this._super.included(app);
 
     this.app.import(app.bowerDirectory + '/ember-simple-auth/simple-auth.amd.js', {
       exports: {
-        'simple-auth-torii': [
-          'authenticators/torii',
-          'initializer'
-        ]
+        'simple-auth-torii/authenticators/torii': ['default'],
+        'simple-auth-torii/initializer':          ['default']
       }
     });
   }
