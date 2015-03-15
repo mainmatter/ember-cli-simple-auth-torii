9ea672403819b72eb96d06055af9f951a75a3fed
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-03-15 21:12:35 +0100

    simplified included hook

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ module.exports = {
   name: 'Ember CLI Simple Auth torii',
 
   included: function(app) {
-    this._super.included(app);
-
-    this.app.import(app.bowerDirectory + '/ember-simple-auth/simple-auth-torii.amd.js', {
+    app.import(app.bowerDirectory + '/ember-simple-auth/simple-auth-torii.amd.js', {
       exports: {
         'simple-auth-torii/authenticators/torii': ['default'],
         'simple-auth-torii/initializer':          ['default']
